7d555571562979503898bb4938493dc75047164f
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2016-02-20 16:34:50 -0500

    working

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {
   firebase: 'finderme',
   mapbox: {
       accessToken: 'pk.eyJ1IjoidTVjZXZyeWRheSIsImEiOiJjaWt2a2toaHEwMGZvdXlrczlzNmNwa2dpIn0.WIYFQtDX2_UX9JRQtP5Qrg',
-      mapId: '',
+      mapId: 'u5cevryday.p76m2med',
   }
 }
