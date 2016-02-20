22a9b4023729a0e01e50c52b414b41da28cd7864
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2016-02-20 15:47:17 -0500

    config js

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+  firebase: 'finderme',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoidTVjZXZyeWRheSIsImEiOiJjaWt2a2toaHEwMGZvdXlrczlzNmNwa2dpIn0.WIYFQtDX2_UX9JRQtP5Qrg',
+      mapId: '',
+  }
+}
