56d535e3b1d681a9e0b84b264eb572ae2b9c54b4
Udara Weerasinghege <udara@eventmobi.com>
2016-09-28 22:02:36 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  firebase: 'finderme',
-  mapbox: {
-      accessToken: 'pk.eyJ1IjoidTVjZXZyeWRheSIsImEiOiJjaWt2a2toaHEwMGZvdXlrczlzNmNwa2dpIn0.WIYFQtDX2_UX9JRQtP5Qrg',
-      mapId: 'u5cevryday.p76m2med',
-  }
-}
